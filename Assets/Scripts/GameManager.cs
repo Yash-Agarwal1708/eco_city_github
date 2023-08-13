e3a8130fb8452a1b89ddf742c8366a65b8a61aed
Yash-Agarwal1708 <yash.agarwal.phy21@iitbhu.ac.in>
2023-08-13 16:09:59 +0530

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using SVS;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public CameraMovement cameraMovement;
+    public InputManager inputManager;
+    public RoadManager roadManager;
+    private void Start()
+    {
+        inputManager.OnMouseClick += roadManager.PlaceRoad;
+        inputManager.OnMouseHold += roadManager.PlaceRoad;
+        inputManager.OnMouseUp += roadManager.FinishPlacingRoad;
+    }
+
+
+    private void Update()
+    {
+        cameraMovement.MoveCamera(new Vector3(inputManager.CameraMovementVector.x, 0, inputManager.CameraMovementVector.y)); 
+    }
+}
---
+++ Assets/Scripts/PlacementManager.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementManager : MonoBehaviour
+{
+    public int width, height;
+    Grid placementGrid;
+    private Dictionary<Vector3Int, StructureModel> temporaryRoadObjects = new Dictionary<Vector3Int, StructureModel>();
+    private Dictionary<Vector3Int, StructureModel> structureDictionary = new Dictionary<Vector3Int, StructureModel>();
+    private void Start()
+    {
+        placementGrid = new Grid(width, height);
+    }
+
+    internal CellType[] GetNeighbourTypesFor(Vector3Int position)
+    {
+        return placementGrid.GetAllAdjacentCellTypes(position.x, position.z);
+    }
+
+    internal bool CheckIfPositionInBound(Vector3Int position)
+    {
+        if(position.x>=0 && position.x<width && position.z>=0 && position.z<height)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    internal bool CheckIfPositionIsFree(Vector3Int position)
+    {
+        return CheckIfPositionIsOfType(position,CellType.Empty);
+    }
+
+    private bool CheckIfPositionIsOfType(Vector3Int position,CellType type)
+    {
+        return placementGrid[position.x,position.z]==type;
+    }
+
+    internal void PlaceTemporaryStructure(Vector3Int position, GameObject structurePrefab, CellType type)
+    {
+        placementGrid[position.x,position.z]=type;
+        //GameObject newStructure = Instantiate(roadStraight, position, Quaternion.identity);
+        StructureModel structure = CreateANewStructureModel(position, structurePrefab, type);
+        temporaryRoadObjects.Add(position, structure);
+    }
+    internal List<Vector3Int> GetNeighboursOfTypeFor(Vector3Int position, CellType type)
+    {
+        var neighbourVertices = placementGrid.GetAdjacentCellsOfType(position.x, position.z, type);
+        List<Vector3Int> neighbours = new List<Vector3Int>();
+        foreach (var point in neighbourVertices)
+        {
+            neighbours.Add(new Vector3Int(point.X, 0, point.Y));
+        }
+        return neighbours;
+    }
+
+    private StructureModel CreateANewStructureModel(Vector3Int position, GameObject structurePrefab, CellType type)
+    {
+        GameObject structure = new GameObject(type.ToString());
+        structure.transform.SetParent(transform);
+        structure.transform.localPosition = position;
+        var structureModel = structure.AddComponent<StructureModel>();
+        structureModel.CreateModel(structurePrefab);
+        return structureModel;
+    }
+    internal List<Vector3Int> GetPathBetween(Vector3Int startPosition, Vector3Int endPosition)
+    {
+        var resultPath = GridSearch.AStarSearch(placementGrid, new Point(startPosition.x, startPosition.z), new Point(endPosition.x, endPosition.z));
+        List<Vector3Int> path = new List<Vector3Int>();
+        foreach (Point point in resultPath)
+        {
+            path.Add(new Vector3Int(point.X,0, point.Y));
+        }
+        return path;
+    }
+
+    internal void RemoveAllTemporaryStructures()
+    {
+        foreach (var structure in temporaryRoadObjects.Values)
+        {
+            var position = Vector3Int.RoundToInt(structure.transform.position);
+            placementGrid[position.x, position.z] = CellType.Empty;
+            Destroy(structure.gameObject);
+        }
+        temporaryRoadObjects.Clear();
+    }
+    internal void AddTemporaryStructuresToStructureDictionary()
+    {
+        foreach (var structure in temporaryRoadObjects)
+        {
+            structureDictionary.Add(structure.Key, structure.Value);
+        }
+        temporaryRoadObjects.Clear();
+    }
+
+    public void ModifyStructureModel(Vector3Int position, GameObject newModel, Quaternion rotation)
+    {
+        if (temporaryRoadObjects.ContainsKey(position))
+            temporaryRoadObjects[position].SwapModel(newModel, rotation);
+        else if (structureDictionary.ContainsKey(position))
+            structureDictionary[position].SwapModel(newModel, rotation);
+    }
+}
